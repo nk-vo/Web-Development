178d01339f8156110c92ce9c8a479faf2bb2a032
nick <vo.nick2686@gmail.com>
2022-03-07 18:35:27 -0800

    Done module 7

---
+++ SQL/dump.sqlite.sql
@@ -0,0 +1,26 @@
+-- TABLE
+CREATE TABLE 'customers'(
+	id INT, first_name string, last_name string, address string,
+  primary key (id)
+);
+CREATE TABLE 'orders' (
+  id int not NULL,
+  order_number int,
+  customer_id int,
+  product_id INT,
+  PRIMARY KEY (id), FOREIGN key (customer_id) REFERENCES customers(id),
+  FOREIGN key (product_id) REFERENCES products(id)
+);
+CREATE TABLE products (
+	id INT NOT NULL,
+    name STRING,
+    price MONEY, stock INT,
+    PRIMARY KEY (ID)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
